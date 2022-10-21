6631e1f73d8ce2be5e56c467046644c009ee12ff
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-10-21 10:09:39 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    # this setter method is responsible for getting the dogs name
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+
+    # this getter method is responsible for setting the dogs name
+    def name
+        @this_dogs_name
+    end
 
 end
+
+lassie = Dog.new
+# getter to set attribute
+lassie.name = "Lassie" # 
+
+# getter to return name
+puts lassie.name
